d93876c0b6dee5aef7bb562462ccf7e31337fc92
P <jp.professional@outlook.com>
2024-03-04 08:52:27 -0300

    feat: add popup with listing acessed websites

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,35 @@
-console.log('background script loaded');
+interface SitesData {
+  [hostname: string]: number;
+}
+
+const sitesData: SitesData = {};
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    try {
+      const url = tab?.url ? new URL(tab.url) : null;
+
+      if (url) {
+        const hostname = url.hostname;
+
+        if (!sitesData[hostname]) {
+          sitesData[hostname] = 0;
+        }
+
+        chrome.storage.local.set({ sitesData });
+      } else {
+        console.error("Invalid URL:", tab?.url);
+      }
+    } catch (error) {
+      console.error("Error processing URL:", tab?.url, error);
+    }
+  });
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get("sitesData", (data) => {
+    if (data && data.sitesData) {
+      Object.assign(sitesData, data.sitesData);
+    }
+  });
+});
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import { useEffect, useState } from "react";
+import logo from "@assets/img/logo.svg";
 
 export default function Popup(): JSX.Element {
+  const [sitesData, setSitesData] = useState({});
+
+  useEffect(() => {
+    chrome.storage.local.get("sitesData", (data) => {
+      console.log(data.sitesData);
+
+      if (data && data.sitesData) {
+        setSitesData(data.sitesData);
+      }
+    });
+  }, []);
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
+        <img
+          src={logo}
+          className="h-36 pointer-events-none animate-spin-slow"
+          alt="logo"
+        />
         <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
+          Edit123 <code>src/pages/popup/Popup.jsx</code> and save to reload.
         </p>
         <a
           className="text-blue-400"
@@ -17,7 +33,13 @@ export default function Popup(): JSX.Element {
         >
           Learn React!
         </a>
-        <p>Popup styled with TailwindCSS!</p>
+        <ul>
+          {Object.entries(sitesData).map(([site, time]) => (
+            <li key={site} className="text-red-700">
+              <strong>{site}:</strong> {time} seconds
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
